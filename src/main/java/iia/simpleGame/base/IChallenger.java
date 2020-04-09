312236a4b5546f329a0320aa1b2d7272400b2752
TheBolshe <bibsim91@gmail.com>
2020-04-09 17:51:06 +0200

    ajouter le contenu du jar

--- src/main/java/iia/simpleGame/base/IChallenger.java
+++ src/main/java/iia/simpleGame/base/IChallenger.java
@@ -1,4 +1,20 @@
-package iia.simpleGame.base;
+package iia.games.base;
 
 public interface IChallenger {
+
+    String teamName();
+
+    void setRole(String role);
+
+    void iPlay(String move);
+
+    void otherPlay(String move);
+
+    String bestMove();
+
+    String victory();
+
+    String defeat();
+
+    String tie();
 }
